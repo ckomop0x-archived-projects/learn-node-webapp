de5af6b73440cd4aa7a7174d0323016f0cbc2d82
Pavel Klochkov <paul.klochkov@gmail.com>
2020-02-22 23:42:31 +0100

    Add pictures upload

--- public/javascripts/modules/autocomplete.js
+++ public/javascripts/modules/autocomplete.js
@@ -1,5 +1,21 @@
 function autocomplete(input, latInput, lngInput) {
     console.log(arguments)
+    if(!input) return;
+
+    const dropdown = new google.maps.places.Autocomplete(input);
+    dropdown.addListener('place_changed', () => {
+        const place = dropdown.getPlace()
+        console.log(place)
+        latInput.value = place.geometry.location.lat()
+        lngInput.value = place.geometry.location.lng()
+    })
+
+    // if someone hits Enter don't submit the form
+    input.on('keydown', (evt) => {
+        if (evt.key === 'Enter') {
+            evt.preventDefault()
+        }
+    })
 }
 
 export default autocomplete;
