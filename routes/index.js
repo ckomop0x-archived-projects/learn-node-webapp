9bf9403e40e5004369235359cfdfaf7c2c2ae129
Pavel Klochkov <paul.klochkov@gmail.com>
2020-02-23 22:17:36 +0100

    Add login flow

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController');
+const userController = require('../controllers/userController');
+const authController = require('../controllers/authController');
 
 const { catchErrors } = require('../handlers/errorHandlers');
-
-// Do work here
 
 // main page
 router.get('/', catchErrors(storeController.getStores));
@@ -38,5 +38,19 @@ router.get('/stores/:id/edit', catchErrors(storeController.editStore));
 // Tags page
 router.get('/tags', catchErrors(storeController.getStoresByTag));
 router.get('/tags/:tag', catchErrors(storeController.getStoresByTag));
+
+// User flow
+router.get('/login', userController.loginForm);
+router.get('/register', userController.registerForm);
+
+// 1. Validate registration data
+// 2. register the user
+// 3. Log in user
+router.post(
+  '/register',
+  userController.validateRegister,
+  userController.register,
+  authController.login
+);
 
 module.exports = router;
