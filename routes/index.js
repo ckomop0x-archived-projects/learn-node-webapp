1f314fa61ab3bd31effbecae77df941f4523076b
Pavel Klochkov <paul.klochkov@gmail.com>
2020-03-08 21:05:38 +0100

    Add location API

--- routes/index.js
+++ routes/index.js
@@ -62,9 +62,12 @@ router.post(
   authController.login
 );
 
+// router.get('/map', storeController.mapPage);
+
 /**
  * API
  */
 router.get('/api/search', catchErrors(storeController.searchStores));
+router.get('/api/stores/near', catchErrors(storeController.mapStores));
 
 module.exports = router;
