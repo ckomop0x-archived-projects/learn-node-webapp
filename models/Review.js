9606a76a84288b68b67c856a216f7268d70f55d6
Pavel Klochkov <paul.klochkov@gmail.com>
2020-03-24 19:20:12 +0100

    Finish reviews

--- models/Review.js
+++ models/Review.js
@@ -4,27 +4,35 @@ mongoose.Promise = global.Promise;
 const reviewSchema = new mongoose.Schema({
   created: {
     type: Date,
-    default: Date.now
+    default: Date.now,
   },
   author: {
     type: mongoose.Schema.ObjectId,
     ref: 'User',
-    required: 'You must supply an author!'
+    required: 'You must supply an author!',
   },
   store: {
     type: mongoose.Schema.ObjectId,
     ref: 'Store',
-    required: 'You must supply an store!'
+    required: 'You must supply an store!',
   },
   text: {
     type: String,
-    required: 'Your review must have a text!'
+    required: 'Your review must have a text!',
   },
-  raiting: {
+  rating: {
     type: Number,
     min: 1,
-    max: 5
-  }
+    max: 5,
+  },
 });
+
+function autopopulate(next) {
+  this.populate('author');
+  next();
+}
+
+reviewSchema.pre('find', autopopulate);
+reviewSchema.pre('findOne', autopopulate);
 
 module.exports = mongoose.model('Review', reviewSchema);
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const storeController = require('../controllers/storeController');
 const userController = require('../controllers/userController');
 const authController = require('../controllers/authController');
+const reviewController = require('../controllers/reviewController');
 
 const { catchErrors } = require('../handlers/errorHandlers');
 
@@ -32,8 +33,19 @@ router.get('/store/:slug', catchErrors(storeController.getStoreBySlug));
 router.get('/stores/:id/edit', catchErrors(storeController.editStore));
 // Map with stores
 router.get('/map', storeController.mapPage);
-router.get('/hearts', authController.isLoggedIn, catchErrors(storeController.heartsPage));
-
+// Liked stores
+router.get(
+  '/hearts',
+  authController.isLoggedIn,
+  catchErrors(storeController.heartsPage)
+);
+// Reviews
+router.post(
+  '/reviews/:id',
+  authController.isLoggedIn,
+  catchErrors(reviewController.addReview)
+);
+router.get('/top', catchErrors(storeController.getTopStores));
 
 // Tags
 router.get('/tags', catchErrors(storeController.getStoresByTag));
@@ -65,7 +77,6 @@ router.post(
   userController.register,
   authController.login
 );
-
 
 /**
  * API
